d43ebb12cd09b084c25c7803cacae224f01b1c91
szgzs <61453326+szgzs@users.noreply.github.com>
2020-03-26 10:45:44 +0100

    Update add_printer_and_set_as_default.ps1

--- add_printer_and_set_as_default.ps1
+++ add_printer_and_set_as_default.ps1
@@ -1,4 +1,4 @@
 Add-Printer -ConnectionName \\server\printer_name
-(New-Object -ComObject WScript.Network).SetDefaultPrinter('HPAC_BW')
+(New-Object -ComObject WScript.Network).SetDefaultPrinter('printer_name')
 $wsObject = New-Object -COM WScript.Network
 $wsObject.SetDefaultPrinter(“\\server\printer_name”)
